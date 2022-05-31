1e7d4f71ee4bc4f78fc05d2aaa8491fc5d70c507
Elias_Dias <eliasdiasdearaujo2@gmail.com>
2022-05-31 12:05:36 -0300

    Atividade 06 concluída

---
+++ atividade06/frontend/src/components/crud/professor/ListProfessor.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+import ProfessorTableRow from "./ProfessorTableRow";
+
+import FirebaseContext from "../../../utils/FirebaseContext";
+import FirebaseProfessorService from "../../../services/FirebaseProfessorService";
+
+const ListProfessorPage = () =>
+<FirebaseContext.Consumer>
+    {
+        (value) => <ListProfessor firebase={value} />
+    }
+</FirebaseContext.Consumer>
+
+function ListProfessor(props) {
+
+    const [professors, setProfessors] = useState([])
+
+    useEffect(
+        () => {
+            FirebaseProfessorService.list_onSnapshot(
+                props.firebase.getFirestoreDb(),
+                (professors)=>{
+                    setProfessors(professors)
+                }
+            )
+        }
+        ,
+        []
+    )
+
+    function deleteProfessorById(_id){
+        let professorsTemp = professors
+        for(let i=0;i<professorsTemp.length;i++){
+            if(professorsTemp[i]._id === _id){
+                //console.log("1")
+                professorsTemp.splice(i,1)
+            }
+        }
+        setProfessors([...professorsTemp])
+
+    }
+
+    function generateTable() {
+
+        if (!professors) return
+        return professors.map(
+            (professor, i) => {
+                return <ProfessorTableRow 
+                    professor={professor} 
+                    key={i} 
+                    deleteProfessorById={deleteProfessorById}
+                    firestore = {props.firebase.getFirestoreDb()}
+                    />
+            }
+        )
+    }
+
+    return (
+        <>
+            <main>
+                <h2>
+                    Listar Professors
+                </h2>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Univeersidade</th>
+                            <th>grau</th>
+                            <th colSpan={2} style={{ textAlign: "center" }}></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {generateTable()}
+                    </tbody>
+                </table>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </>
+    );
+}
+
+export default ListProfessorPage
